753fb006f21057bcbadcc910870619b8e0afcc89
Ravi Teja Nakka <ravitejanakka96@gmail.com>
2026-02-18 19:27:54 +0530

    Day1 Spring Boot setup with PostgreSQL configuration

---
+++ src/main/java/com/shopxkart/shopxkart/ShopxkartApplication.java
@@ -0,0 +1,13 @@
+package com.shopxkart.shopxkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopxkartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopxkartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopxkart/shopxkart/ShopxkartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopxkart.shopxkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopxkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
